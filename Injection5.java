7c132912bdfd6cca1928c148399407bdf5e95c54
anushigupta <anushigupta@users.noreply.github.com>
2024-05-16 16:48:31 +0530

    Create Injection5.java

---
+++ Injection5.java
@@ -0,0 +1,8 @@
+public List<AccountDTO> unsafeJpaFindAccountsByCustomerId(String customerId) {    
+    String jql = "from Account where customerId = '" + customerId + "'";        
+    TypedQuery<Account> q = em.createQuery(jql, Account.class);        
+    return q.getResultList()
+      .stream()
+      .map(this::toAccountDTO)
+      .collect(Collectors.toList());        
+}
